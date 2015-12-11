1756d330c34026b2e4f1e088a173a2b14fe8c39b
Hideo Suzuki <lupinthe14th@gmail.com>
2015-12-11 14:56:28 +0900

    [modify] Add markdown extensions extra and toc

--- pelicanconf.py
+++ pelicanconf.py
@@ -42,7 +42,8 @@ PYGMENTS_STYLE = 'zenburn'
 
 # GitHub Flavored Markdown
 MD_EXTENSIONS = ['linkify', 'del_ins', 'fenced_code',
-                 'codehilite(css_class=highlight)', 'tables']
+                 'codehilite(css_class=highlight)', 'tables',
+                 'extra', 'toc']
 
 # Pelican Plugins setting
 PLUGIN_PATHS = ['./pelican-plugins']
